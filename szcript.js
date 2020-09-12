3991547e6e4aed40f65d2566a35de72eb38f42b6
Catherine Le <lecatherineu@gmail.com>
2020-09-12 16:38:48 -0400

    new

--- szcript.js
+++
@@ -1,6 +0,0 @@
-function drawszlider(ossz, meik){
-	var szazalek=Math.round((meik*100)/ossz);
-	document.getElementById("szliderbar").style.width=szazalek+'%';
-	document.getElementById("szazalek").innerHTML=szazalek+'%';
-	
-}
